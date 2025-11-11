4f869035d40f90e9bb6f14c44056997f54398a6e
hong-seung-kwan <hh8164@naver.com>
2025-11-11 18:58:47 +0900

    [Feat] 프로젝트 환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div style={{padding:"2rem", fontFamily:"sans-serif"}}>
+      <h1>Project Diary</h1>
+    </div>
+  )
+}
+
+export default App
