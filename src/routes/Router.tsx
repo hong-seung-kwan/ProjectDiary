6b2bd2035dca1467783e38622b7c9cb94841c4d2
hong-seung-kwan <hh8164@naver.com>
2025-11-13 16:24:03 +0900

    [Fix] Router 경로 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,7 +4,6 @@ import Homepage from '../pages/Homepage'
 import ProjectAddPage from '../pages/ProjectAddPage'
 import DiaryWritePage from '../pages/DiaryWritePage'
 import ProjectManagePage from '../pages/ProjectManagePage'
-import LoginPage from '../pages/LoginPage'
 import AuthPage from '../pages/AuthPage'
 
 const Router = () => {
@@ -15,7 +14,7 @@ const Router = () => {
       <Route path="/project-add" element={<ProjectAddPage />} />
       <Route path="/diary-write" element={<DiaryWritePage />} />
       <Route path="/project-manage" element={<ProjectManagePage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<AuthPage />} />
       <Route path="/auth" element={<AuthPage />} />
     </Routes>
 
